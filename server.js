c2ba88fde1f65488c63735b59a4c60e861ec0a6f
krishisinnocent <krishsheth963@gmail.com>
2025-07-07 11:09:17 +0530

    backend with postgreSQL and EJS setup

--- server.js
+++ server.js
@@ -2,6 +2,14 @@ const express = require('express');
 const pool = require('./db');
 const PORT = process.env.PORT || 3000;
 const app = express();
+
+// ejs setup
+app.set('view engine', 'ejs');
+
+const path = require('path');
+app.set('views', path.join(__dirname, 'views'))
+
+// Middleware
 app.use(express.json());
 
 app.use((req, res, next)=> {
@@ -11,23 +19,37 @@ app.use((req, res, next)=> {
     next();
 });
 
+// Routes
+// app.get('/', (req, res)=> {
+//     res.render('index', {name: 'KRish'});
+// });
+
+// app.get('/users', async(req, res)=> {
+//     try{
+//         const result = await pool.query
+//         ("UPDATE users SET age = 18 WHERE name = 'nitya';");
+//         res.send('Done');
+//     } catch(error){
+//         console.error('Query failed:', error);
+//         res.status(500).send('Server Error');
+//     }
+// });
+
+// app.get('/result', async(req, res)=>{
+//     const result = await pool.query
+//     ("SELECT * FROM users");
+//     res.json(result.rows);
+// })
+
 app.get('/users', async(req, res)=> {
-    try{
-        const result = await pool.query
-        ("UPDATE users SET age = 18 WHERE name = 'nitya';");
-        res.send('Done');
-    } catch(error){
-        console.error('Query failed:', error);
-        res.status(500).send('Server Error');
-    }
+    const output = await pool.query("SELECT * FROM users;");
+    res.render('index', {output});
+    // output.rows.forEach((row) =>{
+    //     console.log(`Name: ${row.name}, Age: ${row.age}`);
+    // });
 });
 
-app.get('/result', async(req, res)=>{
-    const result = await pool.query
-    ("SELECT * FROM users");
-    res.json(result.rows);
-})
-
+// Start server
 app.listen(PORT, () => {
   console.log(`🟢 Express app ready at http://localhost:${PORT}`);
 });
